07470ba3f9a97617c1223e48935120a43bb10066
eomeom03 <127359995+eomeom03@users.noreply.github.com>
2023-03-21 13:44:31 +0900

    Add files via upload

---
+++ 훕스토리 자바스크립트 1주차.js
@@ -0,0 +1,10 @@
+console.log("Hello, world"); console.log("Hello, JavaScript"); console.log("Hello"); //주석
+console.log('안녕하세요?\n줄바꿈해써요');
+console.log('"정말"그랬니? how\'re you?');
+console.log(typeof 124);
+console.log(1 + 1);
+const a = 1 //const : 변수 지정
+const b = 5
+const c = 2
+console.log(a + b);
+console.log(a == c);
